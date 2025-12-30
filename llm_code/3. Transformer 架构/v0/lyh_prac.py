44324ba7cad5be639463e19ad800d5a5beafcdf4
liyihan12 <liyihan12@liyihan12deMacBook-Pro.local>
2025-12-30 12:36:04 +0800

    更新代码 - 2025-12-30 12:36:04

--- llm_code/3. Transformer 架构/v0/lyh_prac.py
+++ llm_code/3. Transformer 架构/v0/lyh_prac.py
@@ -1,10 +1,11 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
+import math
 # 位置编码
 def computer_freqs_cis(dim,seq_len,theta=10000.0):
     # [dim/2]
-    freq_base = 1.0 / (theta ** (torch.range(0,dim,2)/dim))
+    freq_base = 1.0 / (theta ** (torch.arange(0,dim,2)/dim))
     # [seq_len]
     position = torch.arange(0,seq_len,dtype=torch.float32)
     # [seq_len,dim/2]
@@ -15,15 +16,15 @@ def apply_rope(xq,xk,freqs_cis):
     # xq:[bsz,seq_len,num_head,head_dim]
     # freqs_cis:[seq_len,dim/2]
     bsz,seq_len,num_head,head_dim = xq.shape
-    # freqs_cis:[1,seq_len,1,dim/2]
+    # 🌟 freqs_cis:[1,seq_len,1,dim/2]
     freqs_cis = freqs_cis.reshape(1,seq_len,1,-1)
     # xq_1:[bsz,seq_len,num_head,head_dim/2,2]
     xq_1 = xq.float().reshape(*xq.shape[:-1],-1,2)
     xk_1 = xk.float().reshape(*xk.shape[:-1],-1,2)
-    # xq_2:[bsz,seq_len,num_head,head_dim/2]
+    # 🌟 xq_2:[bsz,seq_len,num_head,head_dim/2]
     xq_2 = torch.view_as_complex(xq_1)
     xk_2 = torch.view_as_complex(xk_1)
-    # xq_rope:[bsz,seq_len,num_head,head_dim/2]
+    # 🌟 xq_rope:[bsz,seq_len,num_head,head_dim/2]
     xq_rope = xq_2 * freqs_cis
     xk_rope = xk_2 * freqs_cis
     # xq_3:[bsz,seq_len,num_head,head_dim/2,2]
@@ -48,3 +49,57 @@ class RMSNorm(nn.Module):
         rms  = torch.rsqrt(variance+self.eps)
         # x.shape [bsz,seq_len,d]
         return x * rms * self.weight
+
+# 注意力机制
+class MultiHeadAttention(nn.Module):
+    def __init__(self,d_model,num_head):
+        super().__init__()
+        self.num_head = num_head
+        self.d_model = d_model
+        self.q = nn.Linear(d_model,d_model)
+        self.k = nn.Linear(d_model,d_model)
+        self.v = nn.Linear(d_model,d_model)
+        self.o = nn.Linear(d_model,d_model)
+
+    def forward(self,x,freq_cis,mask=None):
+        bsz,seqlen,d = x.shape
+        q = self.q(x).reshape(bsz,seqlen,self.num_head,-1)
+        k = self.k(x).reshape(bsz,seqlen,self.num_head,-1)
+        v = self.v(x).reshape(bsz,seqlen,self.num_head,-1)
+        if freq_cis is not None:
+            q,k = apply_rope(q,k,freq_cis)
+        q = q.transpose(1,2)
+        k = k.transpose(1,2)
+        v = v.transpose(1,2)
+        # score.shape [bsz,num_head,seqlen,d]
+        score = torch.matmul(q , k.transpose(-1,-2)/math.sqrt(self.d_model//self.num_head))
+        if mask is not None:
+            # 将掩码矩阵中 mask==0 的位置，赋值 float('-inf')
+            score = score.masked_fill(mask==0,float('-inf'))
+        score = F.softmax(score,dim=-1)
+        output = torch.matmul(score,v).transpose(1,2).reshape(bsz,seqlen,d)
+        output = self.o(output)
+        return output
+
+class SwiGLU_FFN(nn.Module):
+    def __init__(self,d_model,d_fnn=None):
+        super().__init__()
+        if d_fnn is None:
+            d_fnn = 4 * d_model
+        self.gate = nn.Linear(d_model,d_fnn,bias=False)
+        self.up = nn.Linear(d_model,d_fnn,bias=False)
+        self.down = nn.Linear(d_fnn,d_model,bias=False)
+    def forward(self,x):
+        # x shape[bsz,seqlen,d]
+        return self.down((F.silu(self.gate(x)))*(self.up(x)))
+
+
+
+bsz,seq,num_head,d = 2,4,2,8
+x = torch.rand(bsz,seq,d)
+print('x 原始 shape:',x.shape)
+mha = MultiHeadAttention(d,num_head)
+freq_cis = computer_freqs_cis(d//num_head,seq)
+print('x 经过 rope 编码过的注意力机制 shape:',mha(x,freq_cis).shape)
+swi = SwiGLU_FFN(d)
+print('x 经过 FFN shape:',swi(x).shape)
