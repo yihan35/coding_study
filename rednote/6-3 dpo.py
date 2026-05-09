e60ef506c8c0220b6d11de138a4b98dd4476c22a
liyihan <liyihan0358@163.com>
2026-05-09 14:45:45 +0800

    更新代码 - 2026-05-09 14:45:45

--- rednote/6-3 dpo.py
+++ rednote/6-3 dpo.py
@@ -9,16 +9,13 @@ class DPO:
     def dpo_loss(self, policy_chosen_logps, policy_reject_logps, ref_chosen_logps, ref_reject_logps):
         # policy_chosen_logps, policy_reject_logps: [B]
         # ref_chosen_logps, ref_reject_logps: [B]
-        
         # 优答：策略 - 参考
         chosen_logr = policy_chosen_logps - ref_chosen_logps
         # 劣答：策略 - 参考
         reject_logr = policy_reject_logps - ref_reject_logps
-        
         # DPO 核心目标
         logits = self.beta * (chosen_logr - reject_logr)
         loss = -F.logsigmoid(logits)
-        
         return loss.mean()
 
 
