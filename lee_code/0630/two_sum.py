9f1aaa161e0fc00df867ed68420a4d7bf079019d
liyihan <liyihan0358@163.com>
2026-06-30 23:57:36 +0800

    更新代码 - 2026-06-30 23:57:36

---
+++ lee_code/0630/two_sum.py
@@ -0,0 +1,10 @@
+def two_sum(nums,target):
+    dic = {}
+    for i,element in enumerate(nums):
+        if target - element in dic:
+            return([dic[target - element],i])
+        dic[element] = i
+    return []
+print(two_sum([2,7,11,15],9))
+# 两数之和，用哈希的思路
+# 时间复杂度 o(n),空间复杂度 o(n)
